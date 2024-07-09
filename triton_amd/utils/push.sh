d552418b4786c905b2f1062bcaa64c71f6c61372
Alexander Efimov <efimov.alexander@gmail.com>
2024-07-09 14:28:50 +0200

    add --no-verify option in push script

--- triton_amd/utils/push.sh
+++ triton_amd/utils/push.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/bash
+help() {
+  echo "Usage: push [-h] [-n] [-m <message>]"
+  echo "   -h : show help message"
+  echo "   -n : do not perform pre-commit checks"
+  echo "   -m : use given string as commit message"
+}
 
-if [ -n "$1" ]
-then 
-   message="$1"
+PRE_COMMIT=1
+COMMIT_MESSAGE="fix"
+while getopts ":hnm:" option; do
+  case $option in
+    h)help; exit;;
+    n)PRE_COMMIT=0;;
+    m)COMMIT_MESSAGE="$OPTARG";;
+    ?)help; exit;;
+  esac
+done
+
+if ((PRE_COMMIT = 1)); then
+  git commit -a -m"$COMMIT_MESSAGE"
 else
-   message="fix"
+  git commit -a -n -m"$COMMIT_MESSAGE"
 fi
-
-git commit -a -m"$message"
 git push binarman $(git branch --show-current)
 
