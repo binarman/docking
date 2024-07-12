003729166599d53fa5b9e9ef79cb2d787e98aecc
Alexander Efimov <efimov.alexander@gmail.com>
2024-07-12 11:34:00 +0200

    add force push option

--- triton_amd/utils/push.sh
+++ triton_amd/utils/push.sh
@@ -3,15 +3,18 @@ help() {
   echo "Usage: push [-h] [-n] [-m <message>]"
   echo "   -h : show help message"
   echo "   -n : do not perform pre-commit checks"
+  echo "   -f : do a force push"
   echo "   -m : use given string as commit message"
 }
 
 PRE_COMMIT=1
 COMMIT_MESSAGE="fix"
-while getopts ":hnm:" option; do
+FORCE_PUSH=0
+while getopts ":hnfm:" option; do
   case $option in
     h)help; exit;;
     n)PRE_COMMIT=0;;
+    f)FORCE_PUSH=1;;
     m)COMMIT_MESSAGE="$OPTARG";;
     ?)help; exit;;
   esac
@@ -22,5 +25,10 @@ if ((PRE_COMMIT == 1)); then
 else
   git commit -a -n -m"$COMMIT_MESSAGE"
 fi
-git push binarman $(git branch --show-current)
+
+if ((FORCE_PUSH == 1)); then
+  git push -f binarman $(git branch --show-current)
+else
+  git push binarman $(git branch --show-current)
+fi
 
