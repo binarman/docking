e3340f74a3b023beef2bdd942bc3defd85729418
Alexander Efimov <efimov.alexander@gmail.com>
2024-08-20 22:30:55 +0200

    add force pull option

--- triton/utils/pull.sh
+++ triton/utils/pull.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/bash
+
+REMOTE_NAME="binarman"
  
 help() {
-  echo "Usage: push [-h] [-v] <command>"
+  echo "Usage: pull [-h] [-f] [-v] <command>"
   echo "     -h : show help message"
+  echo "     -f : do reset --hard instead of rebase"
   echo "     -v : verbose mode"
   echo "command : possible values are empty or \"build\""
 }
 
 VERBOSE=0
-while getopts ":hv" option; do
+FORCE=0
+while getopts ":hfv" option; do
   case $option in
     h)help; exit;;
+    f)FORCE=1;;
     v)VERBOSE=1;;
     ?)help; exit;;
   esac
@@ -18,20 +23,24 @@ done
 
 shift $((OPTIND-1))
 
-git fetch binarman
+git fetch "$REMOTE_NAME"
 
 DIFF_SIZE="$(git diff | wc -l)"
 
-if [ $DIFF_SIZE -ne 0 ]; then
-  git stash
-fi
+if [ $FORCE -ne 0 ]; then
+  git reset --hard "$REMOTE_NAME"/$(git branch --show-current)
+else
+  if [ $DIFF_SIZE -ne 0 ]; then
+    git stash
+  fi
 
-if ! git rebase binarman/$(git branch --show-current); then
-  exit 1
-fi
+  if ! git rebase "$REMOTE_NAME"/$(git branch --show-current); then
+    exit 1
+  fi
 
-if [ $DIFF_SIZE -ne 0 ] && ! git stash pop; then
-  exit 1
+  if [ $DIFF_SIZE -ne 0 ] && ! git stash pop; then
+    exit 1
+  fi
 fi
 
 if [ "$1" = "build" ]
