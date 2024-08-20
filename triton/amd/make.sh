de035329640913539d9bc71b7cb87dfa289246f1
Alexander Efimov <efimov.alexander@gmail.com>
2024-08-20 19:05:02 +0200

    add container name option to run.sh

--- triton/amd/make.sh
+++ triton/amd/make.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
+IMG_NAME="alefimov-triton-img"
+
 cp -r ../utils .
 cp -r ../config_files .
 
-docker build -t alefimov-triton-img .
+docker build -t "$IMG_NAME" .
 
 rm -r utils
 rm -r config_files
--- triton/nvidia/run.sh
+++ triton/nvidia/run.sh
@@ -3,6 +3,20 @@
 CONT_NAME="alefimov-triton"
 IMG_NAME="alefimov-triton-img"
 
+help() {
+  echo "Usage: push [-h] [-n <container name>]"
+  echo "   -h : show help message"
+  echo "   -n : use non default container name"
+}
+
+while getopts ":hn:" option; do
+  case $option in
+    h)help; exit;;
+    n)CONT_NAME="$OPTARG";;
+    ?)help; exit;;
+  esac
+done
+
 if ! docker start -ai "$CONT_NAME"; then
-  docker run -it --gpus all --network host --name "$CONT_NAME" "$IMG_NAME"
+  docker run -it --network host --device /dev/kfd --device /dev/dri --name "$CONT_NAME" "$IMG_NAME"
 fi
