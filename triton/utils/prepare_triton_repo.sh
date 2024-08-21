9499e5129ba6c0742946f05ea218df8abe6e0f55
Alexander Efimov <efimov.alexander@gmail.com>
2024-08-21 12:27:55 +0200

    improve vscode settings generation

--- triton/utils/prepare_triton_repo.sh
+++ triton/utils/prepare_triton_repo.sh
@@ -1,8 +1,10 @@
 #/usr/bin/bash
 
-if [ ! -e "$1/README.md" ]; then
-  git clone https://github.com/triton-lang/triton "$1"
-  cd "$1"
+FULL_REPO_PATH=$(realpath $1)
+
+if [ ! -e "$FULL_REPO_PATH/README.md" ]; then
+  git clone https://github.com/triton-lang/triton "$FULL_REPO_PATH"
+  cd "$FULL_REPO_PATH"
   git remote rename origin openai
   git remote add binarman https://github.com/binarman/triton
   git fetch binarman
@@ -10,11 +12,17 @@ if [ ! -e "$1/README.md" ]; then
   git fetch rocm
   pre-commit install
   cd python
-  VSCODE_SETTINGS="$1/.vscode/settings.json"
-  mkdir "$1/.vscode"
+  VSCODE_SETTINGS="$FULL_REPO_PATH/.vscode/settings.json"
+  mkdir "$FULL_REPO_PATH/.vscode"
   echo "{\"cmake.configureArgs\" : [" > $VSCODE_SETTINGS
-  ESCAPED_PATH=$(echo "$1" | sed 's/\//\\\//')
-  DEBUG=1 strace -f -e trace=execve -s 100 pip3 install -e. 2>&1 >/dev/null | grep "\"cmake\", \"$1\"" | sed "s/.*\[\"cmake\", \"$ESCAPED_PATH\", \(.*\)\].*/\1/" >> $VSCODE_SETTINGS
+  ESCAPED_REPO_PATH=$(echo "$FULL_REPO_PATH" | sed 's/\//\\\//')
+  DEBUG=1 strace -f -e trace=execve -s 100 pip3 install -e. 2>&1 >/dev/null |
+      grep "\"cmake\", \"$FULL_REPO_PATH\"" |
+      sed "s/.*\[\"cmake\", \"$ESCAPED_REPO_PATH\", \(.*\)\].*/\1/" |
+      sed 's/", "/",\n  "/g' |
+      sed 's/DCMAKE_MAKE_PROGRAM=.*ninja/DCMAKE_MAKE_PROGRAM=ninja/' |
+      sed 's/llvm\/llvm-.*include/llvm_persistent\/include/' |
+      sed 's/llvm\/llvm-.*lib/llvm_persistent\/lib/' >> $VSCODE_SETTINGS
   echo "] }" >> $VSCODE_SETTINGS
 fi
 
