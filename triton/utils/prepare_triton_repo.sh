badd99783293d502ab28fac53a632a783087abfe
Alexander Efimov <efimov.alexander@gmail.com>
2024-09-24 14:27:18 +0200

    build triton even if repo already in place

--- triton/utils/prepare_triton_repo.sh
+++ triton/utils/prepare_triton_repo.sh
@@ -11,21 +11,23 @@ if [ ! -e "$FULL_REPO_PATH/README.md" ]; then
   git remote add rocm https://github.com/ROCm/triton
   git fetch rocm
   pre-commit install
-  cd python
-  VSCODE_SETTINGS="$FULL_REPO_PATH/.vscode/settings.json"
-  mkdir "$FULL_REPO_PATH/.vscode"
-  echo "{\"cmake.configureArgs\" : [" > $VSCODE_SETTINGS
-  ESCAPED_REPO_PATH=$(echo "$FULL_REPO_PATH" | sed 's/\//\\\//')
-  DEBUG=1 strace -f -e trace=execve -s 1000 pip3 install -e. 2>&1 >/dev/null |
-      grep "\"cmake\", \"$FULL_REPO_PATH\"" |
-      sed "s/.*\[\"cmake\", \"$ESCAPED_REPO_PATH\", \(.*\)\].*/\1/" |
-      sed 's/", "/",\n  "/g' |
-      sed 's/DCMAKE_MAKE_PROGRAM=.*ninja/DCMAKE_MAKE_PROGRAM=ninja/' |
-      sed 's/DPYBIND11_INCLUDE_DIR=.*include/DPYBIND11_INCLUDE_DIR=\/root\/.triton\/pybind11\/pybind11-2.13.1\/include\//' |
-      sed 's/llvm\/llvm-.*include/llvm_persistent\/include/' |
-      sed 's/llvm\/llvm-.*lib/llvm_persistent\/lib/' >> $VSCODE_SETTINGS
-  echo "] }" >> $VSCODE_SETTINGS
 fi
 
+cd "$FULL_REPO_PATH/python"
+VSCODE_SETTINGS="$FULL_REPO_PATH/.vscode/settings.json"
+mkdir "$FULL_REPO_PATH/.vscode"
+echo "{\"cmake.configureArgs\" : [" > $VSCODE_SETTINGS
+ESCAPED_REPO_PATH=$(echo "$FULL_REPO_PATH" | sed 's/\//\\\//')
+DEBUG=1 strace -f -e trace=execve -s 1000 pip3 install -e. 2>&1 >/dev/null |
+    grep "\"cmake\", \"$FULL_REPO_PATH\"" |
+    sed "s/.*\[\"cmake\", \"$ESCAPED_REPO_PATH\", \(.*\)\].*/\1/" |
+    sed 's/", "/",\n  "/g' |
+    sed 's/DCMAKE_MAKE_PROGRAM=.*ninja/DCMAKE_MAKE_PROGRAM=ninja/' |
+    sed 's/DPYBIND11_INCLUDE_DIR=.*include/DPYBIND11_INCLUDE_DIR=\/root\/.triton\/pybind11\/pybind11-2.13.1\/include\//' |
+    sed 's/llvm\/llvm-.*include/llvm_persistent\/include/' |
+    sed 's/llvm\/llvm-.*lib/llvm_persistent\/lib/' >> $VSCODE_SETTINGS
+echo "] }" >> $VSCODE_SETTINGS
+
+rm -r ~/.triton/llvm_persistent
 cp -r ~/.triton/llvm/* ~/.triton/llvm_persistent
 
