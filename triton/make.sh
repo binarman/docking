66621019bf6874b161e8aa39f1fd57aa37da8ca3
binarman <efimov.alexander@gmail.com>
2025-01-03 17:24:56 +0300

    make triton container build script directory agnostic

--- triton/make.sh
+++ triton/make.sh
@@ -5,11 +5,13 @@ IMG_NAME="alefimov-triton-img"
 HAS_AMD_GPU=$(test -a /dev/kfd && test -a /dev/dri && echo 1)
 HAS_NVIDIA_GPU=$(test -a /dev/nvidia0 && echo 1)
 
+SCRIPT_DIR=$(dirname "$(realpath -s "$0")")
+
 if [[ $HAS_AMD_GPU = 1 ]]; then
-  docker build -t "$IMG_NAME" -f config_files/dockerfile.amd .
+  docker build -t "$IMG_NAME" -f "$SCRIPT_DIR"/config_files/dockerfile.amd "$SCRIPT_DIR"
 elif [[ $HAS_NVIDIA_GPU = 1 ]]; then
-  docker build -t "$IMG_NAME" -f config_files/dockerfile.nvidia .
+  docker build -t "$IMG_NAME" -f "$SCRIPT_DIR"/config_files/dockerfile.nvidia "$SCRIPT_DIR"
 else
-  docker build -t "$IMG_NAME" -f config_files/dockerfile.nogpu .
+  docker build -t "$IMG_NAME" -f "$SCRIPT_DIR"/config_files/dockerfile.nogpu "$SCRIPT_DIR"
 fi
 
