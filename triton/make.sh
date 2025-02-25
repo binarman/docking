f120d5153fadccd908cdb94220d42ca209909a44
binarman <efimov.alexander@gmail.com>
2025-02-25 16:47:56 +0100

    add option to force type of triton docker image(amd/nvidia/nogpu)

--- triton/make.sh
+++ triton/make.sh
@@ -3,17 +3,25 @@
 SCRIPT_DIR=$(dirname "$(realpath -s "$0")")
 
 source "$SCRIPT_DIR/config_files/user.config"
+source "$SCRIPT_DIR/config_files/utilities.sh"
 
-HAS_AMD_GPU=$(test -a /dev/kfd && test -a /dev/dri && echo 1)
-HAS_NVIDIA_GPU=$(test -a /dev/nvidia0 && echo 1)
+IMAGE_SUFFIX="$(get_device_suffix)"
 
-if [[ $HAS_AMD_GPU = 1 ]]; then
-  IMG_SUFFIX="amd"
-elif [[ $HAS_NVIDIA_GPU = 1 ]]; then
-  IMG_SUFFIX="nvidia"
-else
-  IMG_SUFFIX="nogpu"
-fi
+help() {
+  echo "Usage: make.sh [-h] [-s <override image suffix>]"
+  echo "   -h : show help message"
+  echo "   -s : use non default image suffix: amd, nvidia, nogpu"
+}
 
-docker build -t "$IMAGE_NAME" -f "$SCRIPT_DIR"/config_files/dockerfile.${IMG_SUFFIX} --build-arg USER_NAME="$USER_NAME" --build-arg USER_EMAIL="$USER_EMAIL" --build-arg INITIAL_TRITON_BRANCH="$INITIAL_TRITON_BRANCH" "$SCRIPT_DIR"
+while getopts ":hs:" option; do
+  case $option in
+    h)help; exit;;
+    s)IMAGE_SUFFIX="$OPTARG";;
+    ?)help; exit;;
+  esac
+done
+
+IMAGE_NAME="${IMAGE_BASE_NAME}-${IMAGE_SUFFIX}"
+
+docker build -t "$IMAGE_NAME" -f "$SCRIPT_DIR"/config_files/dockerfile.${IMAGE_SUFFIX} --build-arg USER_NAME="$USER_NAME" --build-arg USER_EMAIL="$USER_EMAIL" --build-arg INITIAL_TRITON_BRANCH="$INITIAL_TRITON_BRANCH" "$SCRIPT_DIR"
 
--- triton/run.sh
+++ triton/run.sh
@@ -2,26 +2,31 @@
 
 SCRIPT_DIR=$(dirname "$(realpath -s "$0")")
 source "$SCRIPT_DIR/config_files/user.config"
+source "$SCRIPT_DIR/config_files/utilities.sh"
 
 help() {
   echo "Usage: run.sh [-h] [-n <container name>]"
   echo "   -h : show help message"
   echo "   -n : use non default container name"
+  echo "   -s : use non default image suffix: amd, nvidia, nogpu"
 }
 
 CONT_NAME="$DEFAUL_CONTAINER_NAME"
 
-while getopts ":hn:" option; do
+IMAGE_SUFFIX="$(get_device_suffix)"
+
+while getopts ":hn:s:" option; do
   case $option in
     h)help; exit;;
     n)CONT_NAME="$OPTARG";;
+    s)IMAGE_SUFFIX="$OPTARG";;
     ?)help; exit;;
   esac
 done
 
+IMAGE_NAME="${IMAGE_BASE_NAME}-${IMAGE_SUFFIX}"
+
 if ! docker start -ai "$CONT_NAME"; then
-  HAS_AMD_GPU=$(test -a /dev/kfd && test -a /dev/dri && echo 1)
-  HAS_NVIDIA_GPU=$(test -a /dev/nvidia0 && echo 1)
   if [[ $HAS_AMD_GPU = 1 ]]; then
     docker run -it --network host --device /dev/kfd --device /dev/dri --name "$CONT_NAME" "$IMAGE_NAME"
   elif [[ $HAS_NVIDIA_GPU = 1 ]]; then
