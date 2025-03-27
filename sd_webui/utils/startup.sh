fbb2d14562b3c34311fd3c708edeecfebf05e3f6
binarman <efimov.alexander@gmail.com>
2025-03-27 11:52:43 +0100

    replace simple bash script with control_services.py, make it listen on external connections, fix few initialization issues

--- sd_webui/utils/startup.sh
+++ sd_webui/utils/startup.sh
@@ -26,14 +26,7 @@ export HF_HUB_DISABLE_TELEMETRY=1
 if [[ $INITIALIZE == 0 ]]; then
   export HF_HUB_OFFLINE=1
   export TRANSFORMERS_OFFLINE=1
-  echo "starting webui"
-  /tools/stable-diffusion-webui/webui.sh -f --listen 2>&1 | tee /tmp/webui.log | grep "Startup time:"&
-  echo "starting kohuya_ss"
-  source /tools/python_3.10_venv/bin/activate
-  /tools/kohya_ss/gui.sh --listen 0.0.0.0 --server_port 7861 --inbrowser &> /tmp/kohuya.log&
-
-  sleep inf
-
+  python /tools/control_services.py
   pkill -g $$
 else
   echo "initialize webui environment"
@@ -41,11 +34,12 @@ else
   source /tools/stable-diffusion-webui/venv/bin/activate
   pip install --upgrade huggingface_hub
   python /tools/download_hf_models.py
+  deactivate
 
   echo "initialize kohuya_ss environment"
   python3.10 -m venv /tools/python_3.10_venv
   source /tools/python_3.10_venv/bin/activate
-  /tools/kohuya_ss/setup.sh
+  /tools/kohya_ss/setup.sh
 fi
 
 
