7679ac5692ea354b4b2d9915b1467859aadbf473
binarman <efimov.alexander@gmail.com>
2026-04-12 15:28:47 +0200

    update backup infra

--- backup_infra/backup_machine/snapshot_backup.sh
+++ backup_infra/backup_machine/snapshot_backup.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+if [ "$#" -ne 1 ]; then
+  echo "Illegal number of parameters"
+  exit 1
+fi
+
 cd "$1"
 added=$(git status --porcelain | grep "^??" | wc -l)
 updated=$(git status --porcelain | grep "^ M" | wc -l)
 deleted=$(git status --porcelain | grep "^ D" | wc -l)
-git add -A
-git commit -m"added files: ${added}
+
+if [ $added != 0 ] || [ $updated != 0 ] || [ $deleted != 0 ]; then
+  git add -A
+  git commit -m"added files: ${added}
 updated files: ${updated}
 deleted files: ${deleted}" -a
+fi
